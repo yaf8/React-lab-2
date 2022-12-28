79ba74686203ebe0ba2416c5f0e640d1abd9e7aa
Yafet Abebe Feleke <yafetema15@gmail.com>
2022-12-28 10:11:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './Components/Card';
+//import Display from './Components/Display';
 
 function App() {
+
+  //return  <Display fName = "Yafet" lName ="Abebe" />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Card">
+      <h1 className="H11"> Task: Add three card element </h1>
+    
+      <Card h2="1st Cards h2" h3="1st card h3" />
+      <Card h2="2nd Cards h2" h3="2st card h3" />
+      <Card h2="3nd Cards h2" h3="3st card h3" />   
     </div>
   );
-}
+
+};
 
 export default App;
---
+++ src/Components/Display.js
@@ -0,0 +1,7 @@
+function Display(props){
+    return <h1> Hello world! 
+        {props.fName}  {props.lName} Welcome to React.
+    </h1>;
+}
+
+export default Display;
